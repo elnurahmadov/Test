b2ca859c701a6638fd4bb8c5f37fe2bc19508f8a
elnurahmadov <eelnur020@gmail.com>
2022-03-13 23:44:39 +0400

    DAO - Repository Packages added

---
+++ src/main/java/com/aykan/dao/employee/impl/EmployeeRepositoryImpl.java
@@ -0,0 +1,75 @@
+package com.aykan.dao.employee.impl;
+
+import com.aykan.dao.employee.EmployeeRepository;
+import com.aykan.domain.employee.Employee;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional
+public class EmployeeRepositoryImpl implements EmployeeRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public boolean saveEmployee(Employee employee) {
+        entityManager.persist(employee);
+        return true;
+    }
+
+    @Override
+    public boolean deleteEmployee(Employee employee) {
+        if (entityManager.contains(employee)) {
+            entityManager.remove(employee);
+        } else {
+            Employee deleteEmployee = findEmployeeById(employee.getEmployeeId());
+            entityManager.remove(deleteEmployee);
+        }
+        return true;
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        Employee updatedEmployee = entityManager.merge(employee);
+        return updatedEmployee;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Employee findEmployeeById(Long employeeId) {
+        return entityManager.createNamedQuery("Employee.findFullById", Employee.class).setParameter("employeeId", employeeId).getSingleResult();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Employee> findEmployees() {
+        return entityManager.createNamedQuery("Employee.findAll", Employee.class).getResultList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Long countEmployee() {
+        return entityManager.createNamedQuery("Employee.count", Long.class).getSingleResult();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Employee> betweenSalaryEmployees(int minSalary, int maxSalary) {
+        return entityManager.createNamedQuery("Employee.betweenSalary", Employee.class)
+                .setParameter("minSalary", minSalary)
+                .setParameter("maxSalary", maxSalary)
+                .getResultList();
+    }
+
+    @Override
+    public List<Employee> findEmployeesFirstMax(int first, int max) {
+        return entityManager.createNamedQuery(("Employee.findAll", Employee.class))
+        .setFirstResult(first)
+                .setMaxResult(max)
+                .getResultList();
+    }
+}
